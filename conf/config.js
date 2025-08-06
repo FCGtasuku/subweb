0f438be0a20bcb6cce1a614ddf22d6b333913cd5
tasuku <tasuku@gmail.com>
2025-08-06 09:09:41 +0800

    My first commit

---
+++ conf/config.js
@@ -0,0 +1,25 @@
+window.config = {
+  // 网站标题
+  siteName: 'Subconverter Web',
+  // API 地址
+  apiUrl: 'https://a.y1z.org',
+  // 短域名服务地址
+  shortUrl: 'https://suo.yt',
+  // 默认远程配置
+  remoteConfigName: '默认配置',
+  // 默认远程配置地址
+  remoteConfigUrl: 'https://cdn.jsdmirror.com/gh/ACL4SSR/ACL4SSR@master/Clash/config/ACL4SSR_Online_Mini_NoAuto.ini',
+  // 首页菜单
+  menuItem: [
+    {
+      title: '首页',
+      link: '/',
+      target: '',
+    },
+    {
+      title: 'ip',
+      link: 'http://34.36.47.246/api/ip',
+      target: '_blank',
+    },
+  ],
+};
